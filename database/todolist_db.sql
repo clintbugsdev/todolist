6e3e508e1f376be8d9b38ad29b0e10676b7e07fa
Clinton <clintbugs.dev@gmail.com>
2017-12-10 12:19:49 +0800

    Database

--- database/todolist_db.sql
+++ database/todolist_db.sql
@@ -16,26 +16,25 @@
 CREATE DATABASE IF NOT EXISTS `todolist_db` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 USE `todolist_db`;
 
--- Dumping structure for table todolist_db.todolist
-CREATE TABLE IF NOT EXISTS `todolist` (
+-- Dumping structure for table todolist_db.items
+CREATE TABLE IF NOT EXISTS `items` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `item` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
   `done` tinyint(1) DEFAULT '0',
   `date_created` datetime DEFAULT NULL,
   `date_updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table todolist_db.todolist: ~1 rows (approximately)
-DELETE FROM `todolist`;
-/*!40000 ALTER TABLE `todolist` DISABLE KEYS */;
-INSERT INTO `todolist` (`id`, `item`, `done`, `date_created`, `date_updated`) VALUES
-	(2, 'sdfasd asdfasd', 1, '2017-12-09 16:11:51', '2017-12-09 22:07:58'),
-	(3, 'yes', 0, '0000-00-00 00:00:00', '2017-12-09 16:13:24'),
-	(4, 'yes', 0, '2017-12-09 09:14:48', '2017-12-09 16:14:48'),
-	(5, 'yess', 1, '2017-12-09 09:15:23', '2017-12-09 22:08:02'),
-	(6, 'yes', 0, '2017-12-09 16:16:34', '2017-12-09 16:16:34');
-/*!40000 ALTER TABLE `todolist` ENABLE KEYS */;
+-- Dumping data for table todolist_db.items: ~4 rows (approximately)
+DELETE FROM `items`;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+INSERT INTO `items` (`id`, `item`, `done`, `date_created`, `date_updated`) VALUES
+	(4, 'Breakfast', 1, '2017-12-09 09:14:48', '2017-12-10 12:18:57'),
+	(12, 'Laundry', 0, '2017-12-10 11:10:09', '2017-12-10 12:19:22'),
+	(13, 'Lunch', 1, '2017-12-10 12:14:58', '2017-12-10 12:18:43'),
+	(14, 'Dinner', 1, '2017-12-10 12:15:04', '2017-12-10 12:18:47');
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
